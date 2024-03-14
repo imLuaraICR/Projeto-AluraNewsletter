22213e2f16cd38f217e6fc746df1f4bc524a870f
Luara Lima Oliveira <luara.oliveira@grupoicr.local>
2024-03-14 15:01:54 -0300

    learning react w/ tailwindcss

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,6 +1,6 @@
 const Article = ({ title, text, tags, image, alt }) => {
   return (
-    <div className="flex-col gap-2 items-center text-center p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg sm:hover:shadow-gray-400 sm:hover:dark:shadow-black sm:hover:shadow-lg">
+    <div className="alura-card">
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
         {title}
       </h3>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,3 @@
-import Input from "../Input/Input";
-
 const Form = ({ onSubmit }) => {
   const safeSubmit = (event) => {
     event.preventDefault();
@@ -13,9 +11,9 @@ const Form = ({ onSubmit }) => {
 
   return (
     <form className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5" onSubmit={safeSubmit}>
-      <Input type="text" required placeholder="Insira seu nome"/>
-      <Input type="email" required placeholder="Insira seu e-mail"/>
-      <button type="submit" className="hover:animate-pulse hover:shadow-sm hover:shadow-gray-500 hover:dark:shadow-black py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full uppercase text-gray-200 w-full max-w-sm">Seguir</button>
+      <input type="text" required placeholder="Insira seu nome" className="alura-input"/>
+      <input type="email" required placeholder="Insira seu e-mail" className="alura-input"/>
+      <button type="submit" className="alura-btn">Seguir</button>
     </form>
   );
 };
